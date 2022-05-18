a9ce891ccb17a9f7f3bc08ed58257c3480dc20be
Flora JULIE <flora.juli88@gmail.com>
2022-05-18 16:18:58 +0200

    page contact
    try to display the text next to the image

--- core_website/lib/screens/contact.dart
+++ core_website/lib/screens/contact.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Contact extends StatefulWidget {
-  const Contact({ Key? key }) : super(key: key);
+  const Contact({Key? key}) : super(key: key);
 
   @override
   State<Contact> createState() => _ContactState();
@@ -10,8 +10,69 @@ class Contact extends StatefulWidget {
 class _ContactState extends State<Contact> {
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
     return Container(
-      
+      color: Colors.grey.shade100,
+      padding: const EdgeInsets.all(50),
+      child: Row(
+          /* crossAxisAlignment: CrossAxisAlignment.start, */
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+                height: screenSize.height * .9,
+                width: screenSize.width,
+                child: Image.asset(
+                  'images/working.png',
+                  alignment: Alignment.bottomLeft,
+                  matchTextDirection: true,
+                  fit: BoxFit.contain,
+                )),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                    clipBehavior: Clip.none,
+                    alignment: Alignment.topRight,
+                    children: [
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: const TextSpan(
+                            text: "ABOUT US \n",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontFamily: "Montserrat-Regular",
+                                color: Color.fromRGBO(217, 39, 39, 1),
+                                wordSpacing: 1,
+                                height: 1.5),
+                            children: [
+                              TextSpan(
+                                  text:
+                                      'Share your idea, we will help you with our service \n',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 40,
+                                      wordSpacing: 1,
+                                      height: 1.5,
+                                      letterSpacing: 1)),
+                              TextSpan(
+                                  text:
+                                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eros porta et consectetur auctor gravida mauris tempus pellentesque. Nulla ornare potenti in eu in commodo magna amet. Eros porta et consectetur auctor gravida mauris tempus pellentesque. Nulla ornare potenti in eu in commodo magna amet. ',
+                                  style: TextStyle(
+                                      decoration: TextDecoration.none,
+                                      color: Colors.black,
+                                      fontFamily: 'Monserrat',
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.normal,
+                                      letterSpacing: 1))
+                            ]),
+                      ),
+                    ]),
+              ],
+            )
+          ]),
     );
   }
 }
