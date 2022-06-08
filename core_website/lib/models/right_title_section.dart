cbe161b2e9bf701a1345ec0b24c88489463c191a
Flora <flora.julie88@gmail.com>
2022-06-08 12:35:49 +0200

    start of the optimization of the about page

---
+++ core_website/lib/models/right_title_section.dart
@@ -0,0 +1,34 @@
+import 'package:core_website/config/themes/colors_theme.dart';
+import 'package:flutter/material.dart';
+
+class RightTitleSection extends StatelessWidget {
+  final String title;
+  final String description;
+  const RightTitleSection({Key? key, required this.title, required this.description}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+        overflow: TextOverflow.ellipsis,
+        maxLines: 10,
+        textAlign: TextAlign.justify,
+        text: TextSpan(
+            text: title,
+            style: const TextStyle(
+                fontSize: 18,
+                fontFamily: "Montserrat",
+                color: ColorsTheme.primaryColor,
+                wordSpacing: 1,
+                height: 1.5),
+            children: [
+              TextSpan(
+                text: description,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 40,
+                ),
+              ),
+            ]));
+  }
+}
