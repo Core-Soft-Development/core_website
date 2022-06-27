4fa8fc02ab2af09af11440c453f89492d98d377e
Flora <flora.julie88@gmail.com>
2022-06-27 10:38:48 +0200

    The pages are responsive
    All that's missing is the right design for the different sizes

--- lib/screens/footer.dart
+++ lib/screens/footer.dart
@@ -9,91 +9,88 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class Footer extends StatelessWidget {
   const Footer({Key? key}) : super(key: key);
 
-  static const  faIcon = [
+  static const faIcon = [
     FaIcon(FontAwesomeIcons.facebookSquare, color: Colors.white),
     FaIcon(FontAwesomeIcons.twitter, color: Colors.white),
     FaIcon(FontAwesomeIcons.instagram, color: Colors.white),
     FaIcon(FontAwesomeIcons.linkedinIn, color: Colors.white)
-    ];
+  ];
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
       height: FrameSize.screenHeight / 3,
       color: Colors.black,
       child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 140),
           child: Row(
-            mainAxisSize: MainAxisSize.min,
             children: [
-            Flexible(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                spacerFooter(),
-                Container(
-                    alignment: Alignment.topLeft,
-                    height: FrameSize.screenHeight / 15,
-                    child: Image.asset(
-                      'assets/logos/core_soft_development.png',
-                      color: Colors.white,
-                    )),
-                const Text(
-                  'Lorem ipsum dolor sit amet, consy ect etur adipisc de elit. Quisque act raqum nunc no dolor sit de amet.',
-                  softWrap: true,
-                  overflow: TextOverflow.visible,
-                  style: TextStyle(
-                      color: ColorsTheme.textFooter,
-                      fontFamily: 'Monserrat',
-                      fontWeight: FontWeight.w600,
-                      fontSize: 12,
-                      height: 1),
-                )
-              ]),
-            ),
-            const Spacer(),
-            Expanded(
-              child: textFooter(' Our service', 'How it works',
-                  'Plan & pricing', 'Our works'),
-            ),
-            const Spacer(),
-            Expanded(
-              child: textFooter(
-                  ' Company', 'Blog', 'Contact us', 'Book a call'),
-            ),
-            const Spacer(),
-            Expanded(
-              child: SizedBox(
+              Flexible(
+                child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  spacerFooter(),
+                  Container(
+                      alignment: Alignment.topLeft,
+                      height: FrameSize.screenHeight / 15,
+                      child: Image.asset(
+                        'assets/logos/core_soft_development.png',
+                        color: Colors.white,
+                      )),
+                  const Expanded(
+                    child: Text(
+                      'Lorem ipsum dolor sit amet, consy ect etur adipisc de elit. Quisque act raqum nunc no dolor sit de amet.',
+                      softWrap: true,
+                      overflow: TextOverflow.visible,
+                      style: TextStyle(
+                          color: ColorsTheme.textFooter,
+                          fontFamily: 'Monserrat',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 12,
+                          height: 1),
+                    ),
+                  )
+                ]),
+              ),
+              const Spacer(),
+              Expanded(
+                child: textFooter(' Our service', 'How it works', 'Plan & pricing',
+                    'Our works'),
+              ),
+              const Spacer(),
+              Expanded(child: textFooter(' Company', 'Blog', 'Contact us', 'Book a call')),
+              const Spacer(),
+              SizedBox(
                 width: FrameSize.screenWidth / 6,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text('Support',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Monserrat',
-                            fontSize: 22,
-                            fontWeight: FontWeight.w700,
-                            height: 2)),
-                    const Text(
-                        'Quisque actraqum nunc no dolor sit ametaugue dolor. Lorem ipsum dolor sit amet, consyect etur.',
-                        softWrap: true,
-                        overflow: TextOverflow.visible,
-                        style: TextStyle(
-                            color: ColorsTheme.textFooter,
-                            fontFamily: 'Monserrat',
-                            fontWeight: FontWeight.w300,
-                            fontSize: 16,
-                            height: 1)),
+                    const Expanded(
+                      child: Text('Support',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Monserrat',
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                              height: 2)),
+                    ),
+                    const Expanded(
+                      child: Text(
+                          'Quisque actraqum nunc no dolor sit ametaugue dolor. Lorem ipsum dolor sit amet, consyect etur.',
+                          softWrap: true,
+                          overflow: TextOverflow.visible,
+                          style: TextStyle(
+                              color: ColorsTheme.textFooter,
+                              fontFamily: 'Monserrat',
+                              fontWeight: FontWeight.w300,
+                              fontSize: 16,
+                              height: 1)),
+                    ),
                     spacerFooter(),
-                    const IconSocial(faIcon: faIcon),
+                    const Expanded(child: IconSocial(faIcon: faIcon)),
                   ],
                 ),
-              ),
-            ),
-          ])),
+              )
+            ],
+          )),
     );
   }
 }
-
--- lib/widgets/text_footer.dart
+++ lib/widgets/text_footer.dart
@@ -5,13 +5,15 @@ Column textFooter(String title, String text, String text1, String text2) {
     return Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(title,
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Monserrat',
-                            fontSize: 22,
-                            fontWeight: FontWeight.w700,
-                            height: 2)),
+                    Expanded(
+                      child: Text(title,
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Monserrat',
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                              height: 2)),
+                    ),
                     const SizedBox(height: 20),
                     Expanded(child: footerTextButton(text)),
                     const SizedBox(height: 20),
